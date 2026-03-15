beb023adb54bdcd94af17a2bf97de1bbb3a66466
sugar <engineerzjl@foxmail.com>
2026-03-15 11:05:36 +0800

    feat(ui-ux-pro-max): 新增UI/UX专业搜索工具与设计系统数据

--- packages/server/src/lib/file.ts
+++ packages/server/src/lib/file.ts
@@ -5,7 +5,7 @@ export function formatSize(
 ) {
   let unit
   units = units || ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-  // eslint-disable-next-line no-cond-assign
+   
   while ((unit = units.shift()) && size > 1024) {
     size /= 1024
   }
