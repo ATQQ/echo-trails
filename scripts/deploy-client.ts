1a430690a2c8aeb44a48d0f78593c903bdeb2d5e
sugar <engineerzjl@foxmail.com>
2026-08-13 17:57:44 +0800

    feat: add Kite configuration files and update deployment scripts
    1. Introduced kite.config.prod.json for both app and server with project-specific settings.
    2. Updated deploy-client.ts and deploy-server.ts scripts to utilize Kite for deployment, replacing previous tar and scp commands.
    3. Enhanced deployment process for both client and server environments.

--- scripts/deploy-client.ts
+++ scripts/deploy-client.ts
@@ -1,23 +1,6 @@
 import { $ } from 'bun'
-// user config
-const originName = 'photo'
 
-// not care
-const compressPkgName = `${originName}.tar.gz`
-const user = 'root'
-const origin = 'sugarat.top'
-const fullOrigin = `${originName}.${origin}`
-const baseServerDir = '/www/wwwroot'
-const destDir = ''
+await $`bun run build:client`
 
-await $`npm run build:client`
-
-console.log('压缩生成', compressPkgName);
-await $`cd packages/app && tar -zvcf ${compressPkgName} dist`
-
-console.log('上传压缩包到服务器');
-await $`cd packages/app && scp ${compressPkgName} ${user}@${fullOrigin}:./`
-await $`cd packages/app && rm -rf ${compressPkgName}`
-
-console.log('解压到服务器');
-await $`ssh -p22 ${user}@${fullOrigin} "tar -xf ${compressPkgName} -C ${baseServerDir}/${fullOrigin}/${destDir}"`
+console.log('Kite 部署前端')
+await $`cd packages/app && kite push --env prod`
